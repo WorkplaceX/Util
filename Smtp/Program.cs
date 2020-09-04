5ef73b2d3ef275327506b26ece8737dbae0808b4
WorkplaceX <info@workplacex.com>
2020-09-04 09:27:03 +0200

    Smtp

---
+++ Smtp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new SmtpClient("smtp.gmail.com", 587))
+            {
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("MyEmail", "MyPassword");
+                using (var mailMessage = new MailMessage())
+                {
+                    mailMessage.From = new MailAddress("MyEmail", "MyEmail Display");
+                    mailMessage.To.Add(new MailAddress("Email"));
+                    mailMessage.Subject = "Test Email from SMTP";
+                    mailMessage.Body = "This is the text...";
+
+                    client.Send(mailMessage);
+                }
+            }
+        }
+    }
+}
---
+++ Smtp/Readme.md
@@ -0,0 +1,7 @@
+# Send Email with Gmail SMTP
+
+Send Gmail email with .NET Core C# console app. Turn on Gmail "Less secure app access"
+
+https://myaccount.google.com/lesssecureapps
+
+![Less secure app access](Doc/LessSecureAppAccess.png)
